e3d7bfc56c249b828a2636bdd1736dfa7e6fb73a
Vatsal Raina <vatsalraina@hotmail.co.uk>
2024-02-17 21:05:35 +0000

    zero shot setup

---
+++ qarag/zero-shot/question_embedder.py
@@ -0,0 +1,28 @@
+
+import json
+import argparse
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Get all command line arguments.')
+parser.add_argument('--data_dir', type=str, default='', help='Specify the path to the data directory.')
+parser.add_argument('--embedder', type=str, default="sentence-t5-base", help='Specify the path to the data directory.')
+
+
+def main(args):
+
+    model = SentenceTransformer("sentence-transformers/" + args.embedder)
+
+    print("Started embedding chunks.")
+    with open(args.data_dir + 'questions.json', 'r') as f:
+        questions = json.load(f)
+    question_embeddings = np.asarray(model.encode(questions))
+    with open(args.data_dir + 'questions_' + args.embedder + '.npy', 'wb') as f:
+        np.save(f, question_embeddings)
+    print("Finished embedding questions.")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
+
