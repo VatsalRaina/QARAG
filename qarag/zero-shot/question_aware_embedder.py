c9bc77dd861f165efa97acfec205850fc495a150
Vatsal Raina <vatsalraina@hotmail.co.uk>
2024-02-23 11:57:57 +0000

    multi question support

--- qarag/zero-shot/question_aware_embedder.py
+++ qarag/zero-shot/question_aware_embedder.py
@@ -7,30 +7,47 @@ import numpy as np
 parser = argparse.ArgumentParser(description='Get all command line arguments.')
 parser.add_argument('--data_dir', type=str, default='', help='Specify the path to the data directory.')
 parser.add_argument('--embedder', type=str, default="sentence-t5-base", help='Specify the path to the data directory.')
+parser.add_argument('--qu_count', type=int, default=1, help='Specify the path to the data directory.')
 
 
 def main(args):
 
     model = SentenceTransformer("sentence-transformers/" + args.embedder)
 
-    print("Started embedding questions.")
-    with open(args.data_dir + 'gen_questions_aware.json', 'r') as f:
-        questions = json.load(f)
-    unwrapped_questions = []
-    qu_idx_to_chunk_idx = []
-    for count, question_set in enumerate(questions):
-        unwrapped_questions.extend(question_set)
-        curr_idxs = [count] * len(question_set)
-        qu_idx_to_chunk_idx.extend(curr_idxs)
+    if args.qu_count == 1:
 
-    print(len(unwrapped_questions))
-    question_embeddings = np.asarray(model.encode(unwrapped_questions))
-    with open(args.data_dir + 'questions_aware_' + args.embedder + '.npy', 'wb') as f:
-        np.save(f, question_embeddings)
-    print("Finished embedding questions.")
+        print("Started embedding questions.")
+        with open(args.data_dir + 'gen_questions_aware.json', 'r') as f:
+            questions = json.load(f)
+        unwrapped_questions = []
+        qu_idx_to_chunk_idx = []
+        for count, question_set in enumerate(questions):
+            unwrapped_questions.extend(question_set)
+            curr_idxs = [count] * len(question_set)
+            qu_idx_to_chunk_idx.extend(curr_idxs)
 
-    with open(args.data_dir + 'questions_aware_mapping'+ '.npy', 'wb') as f:
-        np.save(f, np.asarray(qu_idx_to_chunk_idx))
+        print(len(unwrapped_questions))
+        question_embeddings = np.asarray(model.encode(unwrapped_questions))
+        with open(args.data_dir + 'questions_aware_' + args.embedder + '.npy', 'wb') as f:
+            np.save(f, question_embeddings)
+        print("Finished embedding questions.")
+
+    else:
+
+        print("Started embedding questions.")
+        with open(args.data_dir + 'gen_questions_aware_' + str(args.qu_count) + '.json', 'r') as f:
+            questions = json.load(f)
+        unwrapped_questions = []
+        for count, question_set in enumerate(questions):
+            unwrapped_questions.extend(question_set)
+        question_embeddings = np.asarray(model.encode(unwrapped_questions))
+        with open(args.data_dir + 'questions_aware_' + str(args.qu_count) + '_' + args.embedder + '.npy', 'wb') as f:
+            np.save(f, question_embeddings)
+        print("Finished embedding questions.")
+
+    if args.qu_count == 1:
+        with open(args.data_dir + 'questions_aware_mapping'+ '.npy', 'wb') as f:
+            np.save(f, np.asarray(qu_idx_to_chunk_idx))
 
 
 
