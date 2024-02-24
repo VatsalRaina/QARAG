24181ef0b422c7817b51d24f8e46f983a0523a6a
Vatsal Raina <vatsalraina@hotmail.co.uk>
2024-02-24 13:51:06 +0000

    plot sentences count per chunk distribution

---
+++ qarag/visuals/plot_sentence_distribution.py
@@ -0,0 +1,30 @@
+import argparse
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Get all command line arguments.')
+parser.add_argument('--data_dir', type=str, default='', help='Specify the path to the data directory.')
+parser.add_argument('--save_path', type=str, default='', help='Specify the path to save the figure.')
+
+
+def main(args):
+
+    with open(args.data_dir + 'chunks.json', 'r') as f:
+        chunks = json.load(f)
+
+    sentence_counts = []
+    for chunk in chunks:
+        sentences = chunk.split('.')
+        sentence_counts.append(len(sentences))
+    sentence_counts = np.asarray(sentence_counts)
+
+    sns.boxplot(sentence_counts)
+    plt.xlabel('Sentences per chunk')
+    plt.savefig(args.save_path)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
