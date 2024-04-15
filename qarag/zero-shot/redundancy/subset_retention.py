05b1756c37471a65f72fdf9e2859df6538da8ddb
Vatsal Raina <vatsalraina@hotmail.co.uk>
2024-04-15 21:07:17 +0100

    pairwise question cosine distances

--- qarag/zero-shot/redundancy/subset_retention.py
+++ qarag/zero-shot/redundancy/subset_retention.py
@@ -8,10 +8,8 @@ import argparse
 parser = argparse.ArgumentParser(description='Get all command line arguments.')
 parser.add_argument('--data_dir', type=str, default='', help='Specify the path to the data directory.')
 parser.add_argument('--embedder', type=str, default="sentence-t5-base", help='Specify the model name used to search for correct files.')
-parser.add_argument('--qu_count', type=int, default=1, help='Specify the path to the data directory.')
 parser.add_argument('--save_dir', type=str, default='', help='Path to save all cosine distance matrices.')
 parser.add_argument('--K', type=int, default=1, help='Recall depth.')
-
 
 
 def select_subset(matrix, tau):
@@ -81,42 +79,60 @@ def main(args):
 
     assert len(qu_idx_to_chunk_idx) == len(all_question_embeddings)
 
+    out = {
+        'num_questions': [],
+        'recalls': []
+        }
+
     # Cut-off threshold for similarity
-    tau = 0.9
+    taus = [0.7, 0.75, 0.81, 0.9, 0.95, 0.98, 1.0]
 
-    for i in range(len(all_question_embeddings)):
-        print(i, len(all_question_embeddings))
-        arr = all_question_embeddings[i]
-        # Normalize each row
-        arr_normalized = arr / np.linalg.norm(arr, axis=1)[:, np.newaxis]
-        # Calculate cosine similarity using matrix multiplication
-        cosine_similarities = np.dot(arr_normalized, arr_normalized.T)
-        idxs = select_subset(cosine_similarities, tau)
-        retained_qu_idx_to_chunk_idx = np.asarray( [ qu_idx_to_chunk_idx[i] ] * len(idxs) )
-        retained_question_embeddings = arr[idxs]
+    for tau in taus:
+        print("Tau:", tau)
+        for i in range(len(all_question_embeddings)):
+            #print(i, len(all_question_embeddings))
+            arr = all_question_embeddings[i]
+            # Normalize each row
+            arr_normalized = arr / np.linalg.norm(arr, axis=1)[:, np.newaxis]
+            # Calculate cosine similarity using matrix multiplication
+            cosine_similarities = np.dot(arr_normalized, arr_normalized.T)
+            idxs = select_subset(cosine_similarities, tau)
+            retained_qu_idx_to_chunk_idx = np.asarray( [ qu_idx_to_chunk_idx[i] ] * len(idxs) )
+            retained_question_embeddings = arr[idxs]
 
-        if i==0:
-            efficient_qu_idx_to_chunk_idx = retained_qu_idx_to_chunk_idx
-            efficient_question_embeddings = retained_question_embeddings
-        else:
-            efficient_qu_idx_to_chunk_idx = np.concatenate([efficient_qu_idx_to_chunk_idx, retained_qu_idx_to_chunk_idx], axis=0)
-            efficient_question_embeddings = np.concatenate([efficient_question_embeddings, retained_question_embeddings], axis=0)
+            if i==0:
+                efficient_qu_idx_to_chunk_idx = retained_qu_idx_to_chunk_idx
+                efficient_question_embeddings = retained_question_embeddings
+            else:
+                efficient_qu_idx_to_chunk_idx = np.concatenate([efficient_qu_idx_to_chunk_idx, retained_qu_idx_to_chunk_idx], axis=0)
+                efficient_question_embeddings = np.concatenate([efficient_question_embeddings, retained_question_embeddings], axis=0)
 
-    efficient_question_embeddings = torch.from_numpy(efficient_question_embeddings)
+        efficient_question_embeddings = torch.from_numpy(efficient_question_embeddings)
 
-    # Find closest embeddings for each query (using cosine distance)
-    min_indices = get_neighbours(efficient_question_embeddings, query_embeddings, args.K * 225)
-    chunk_indices = efficient_qu_idx_to_chunk_idx[min_indices]
+        # Find closest embeddings for each query (using cosine distance)
+        min_indices = get_neighbours(efficient_question_embeddings, query_embeddings, args.K * 225)
+        chunk_indices = efficient_qu_idx_to_chunk_idx[min_indices]
 
-    hits = 0
-    for count, label in enumerate(labels):
-        curr_chunk_idxs = pd.unique(chunk_indices[count])[:args.K]
-        if label in curr_chunk_idxs: hits += 1
-    print("Recall at:", args.K)
-    print(hits/len(labels))
-    print("Efficient questions:", len(efficient_question_embeddings))
-    print("Maximum number of questions:", len(all_question_embeddings) * len(all_question_embeddings[0]))
-    print("-----------------------")
+        hits = 0
+        for count, label in enumerate(labels):
+            curr_chunk_idxs = pd.unique(chunk_indices[count])[:args.K]
+            if label in curr_chunk_idxs: hits += 1
+        print("Recall at:", args.K)
+        print(hits/len(labels))
+        print("Efficient questions:", len(efficient_question_embeddings))
+        print("Maximum number of questions:", len(all_question_embeddings) * len(all_question_embeddings[0]))
+        print("-----------------------")
+
+        out['num_questions'].append(len(efficient_question_embeddings))
+        out['recalls'].append(hits/len(labels))
+
+    with open(args.save_dir + '/recall_' + args.K + '.json') as f:
+        json.dump(out, f)
+
+
+
+
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
