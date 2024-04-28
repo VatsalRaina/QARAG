5107a8c527ba6b518306f223043b684903d2b4ee
Vatsal Raina <vatsalraina@hotmail.co.uk>
2024-04-28 02:17:53 +0100

    atom aware question generation

---
+++ qarag/zero-shot/question_atom_aware_embedder.py
@@ -0,0 +1,40 @@
+
+import json
+import argparse
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Get all command line arguments.')
+parser.add_argument('--data_dir', type=str, default='', help='Specify the path to the data directory.')
+parser.add_argument('--embedder', type=str, default="sentence-t5-base", help='Specify the path to the data directory.')
+
+
+def main(args):
+
+    model = SentenceTransformer("sentence-transformers/" + args.embedder)
+
+    print("Started embedding atom aware questions.")
+
+    with open(args.data_dir + 'gen_questions_atom_aware.json', 'r') as f:
+            all_chunks_atoms_questions = json.load(f)
+
+    all_questions = []
+    for chunk_atoms in all_chunks_atoms_questions:
+        all_questions.extend(chunk_atoms)
+
+    print("Total number of atom aware questions:", len(all_questions))
+    
+    question_embeddings = np.asarray(model.encode(all_questions))
+    with open(args.data_dir + 'questions_atom_aware_' + args.embedder + '.npy', 'wb') as f:
+        np.save(f, question_embeddings)
+
+    print("Finished embedding atom aware questions.")
+
+
+
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
+
