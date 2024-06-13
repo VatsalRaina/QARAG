f810dbb0393546b03e3fb062c913aa2276bdced4
Vatsal Raina <vatsalraina@hotmail.co.uk>
2024-06-13 16:03:50 +0100

    clapnq

--- qarag/zero-shot/question_atom_aware_evaluator.py
+++ qarag/zero-shot/question_atom_aware_evaluator.py
@@ -39,27 +39,40 @@ def main(args):
     question_embeddings = np.load(args.data_dir + 'questions_atom_aware_' + args.embedder + '.npy')
     atom_idx_to_chunk_idx = np.load(args.data_dir + 'atoms_mapping'+ '.npy')
 
-    if args.qu_count > 1:
-        for count in range(2, args.qu_count+1):
-            curr_question_embeddings = np.load(args.data_dir + 'questions_atom_aware_' + str(count) + '_' + args.embedder + '.npy')
-            question_embeddings = np.concatenate([question_embeddings, curr_question_embeddings], axis=0)
-        atom_idx_to_chunk_idx = np.tile(atom_idx_to_chunk_idx, args.qu_count)
+    # if args.qu_count > 1:
+    #     for count in range(2, args.qu_count+1):
+    #         curr_question_embeddings = np.load(args.data_dir + 'questions_atom_aware_' + str(count) + '_' + args.embedder + '.npy')
+    #         question_embeddings = np.concatenate([question_embeddings, curr_question_embeddings], axis=0)
+    #     atom_idx_to_chunk_idx = np.tile(atom_idx_to_chunk_idx, args.qu_count)
 
-    question_embeddings = torch.from_numpy(question_embeddings)
+    #TEMP
+    for count in range(2, args.qu_count+1):
+        question_embeddings = torch.from_numpy( np.load(args.data_dir + 'questions_atom_aware_' + str(count) + '_' + args.embedder + '.npy') )
+        min_indices = get_neighbours(question_embeddings, query_embeddings, args.K * 25 * args.qu_count)
+        chunk_indices = atom_idx_to_chunk_idx[min_indices]
+        hits = 0
+        for count, label in enumerate(labels):
+            curr_chunk_idxs = pd.unique(chunk_indices[count])[:args.K]
+            if label in curr_chunk_idxs: hits += 1
+        print(hits/len(labels))
+    ###
 
-    # Find closest embeddings for each query (using cosine distance)
-    min_indices = get_neighbours(question_embeddings, query_embeddings, args.K * 25 * args.qu_count)
-    chunk_indices = atom_idx_to_chunk_idx[min_indices]
 
-    hits = 0
-    print(labels)
-    print([ch[0] for ch in chunk_indices])
-    for count, label in enumerate(labels):
-        curr_chunk_idxs = pd.unique(chunk_indices[count])[:args.K]
-        if label in curr_chunk_idxs: hits += 1
-        if len(pd.unique(chunk_indices[count])) < args.K: print(count, "Something is wrong")
-    print("Recall at ", args.K)
-    print(hits/len(labels))
+    # question_embeddings = torch.from_numpy(question_embeddings)
+
+    # # Find closest embeddings for each query (using cosine distance)
+    # min_indices = get_neighbours(question_embeddings, query_embeddings, args.K * 25 * args.qu_count)
+    # chunk_indices = atom_idx_to_chunk_idx[min_indices]
+
+    # hits = 0
+    # print(labels)
+    # print([ch[0] for ch in chunk_indices])
+    # for count, label in enumerate(labels):
+    #     curr_chunk_idxs = pd.unique(chunk_indices[count])[:args.K]
+    #     if label in curr_chunk_idxs: hits += 1
+    #     if len(pd.unique(chunk_indices[count])) < args.K: print(count, "Something is wrong")
+    # print("Recall at ", args.K)
+    # print(hits/len(labels))
 
 if __name__ == "__main__":
     args = parser.parse_args()
